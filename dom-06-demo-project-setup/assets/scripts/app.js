13f39c3ecda3b8358320d90e572dfd2fad84565b
sehwa <love7620640@naver.com>
2023-08-08 19:36:52 +0900

    <study> input clear

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,1 +1,74 @@
-// dom
+//] DOM
+//< 입력
+const addMovieModal = document.getElementById("add-modal"); //< 성능 측면에서 가장 빠른 방법
+// const addMovieModal = document.body.children[1];
+
+const startAddMovieButton = document.querySelector("header button");
+// const startAddMovieButton = document.querySelector("header").lastElementChild;
+
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+
+//< 저장
+const movies = [];
+
+//] function
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+  clearMovieInput();
+};
+
+const clearMovieInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = "";
+  }
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imgUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5)");
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imgUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
+  toggleMovieModal();
+  clearMovieInput();
+};
+
+// eventListener 버튼 클릭해야 호출되는 함수는 handler 붙이기
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
