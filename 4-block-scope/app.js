8896add72d1353fc0e4ddb4ba879a22411f6943e
sehwa <love7620640@naver.com>
2023-08-22 19:30:59 +0900

    feat: heap & stack

--- 4-block-scope/app.js
+++ 4-block-scope/app.js
@@ -1,14 +1,20 @@
-let name = 'Max';
+let name = "Max";
 
-if (name === 'Max') {
-  let hobbies = ['Sports', 'Cooking'];
+if (name === "Max") {
+  let hobbies = ["Sports", "Cooking"];
   console.log(hobbies);
-
 }
+
+//; var
+//-- 범위 : Function-scoped
+
+//; let, const
+//-- 범위 :  {Block-scoped}
+// { 중괄호 } —>{} 내에서만 선언된 변수를 사용할 수 있음. (조건문, 반복문 함수)
 
 function greet() {
   let age = 30;
-  let name = 'Manuel';
+  let name = "Manuel";
   console.log(name, age, hobbies);
 }
 
---
+++ 5-heap-stack/app.js
@@ -0,0 +1,8 @@
+function getName() {
+  return prompt("Your name: ", "");
+}
+
+function greet() {
+  const userName = getName();
+  console.log("Hello", userName);
+}
